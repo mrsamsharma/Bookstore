94697a2bdc0d12efe43b207caa8d500abe1e55e4
Satyam Sharma <samwfelice@gmail.com>
2022-12-07 06:17:28 -0800

    Create AddBook Component

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
 import Book from '../components/Book';
+import AddBook from '../components/AddBook';
 
 export default function Books() {
   return (
     <>
       <Header />
       <Book title="The Art of Seduction" author="Robert Gren" />
+      <AddBook />
     </>
   );
 }
--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -4,4 +4,4 @@ const Categories = () => (
     <button>Check Status</button>
 )
 
-export default Categories
+export default Categories;
