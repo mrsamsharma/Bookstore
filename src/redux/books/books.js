a039fb292928f171cde8206184d7acccb6605ea6
Satyam Sharma <samwfelice@gmail.com>
2022-12-15 06:52:00 -0800

    Create Action Creator for ADD and REMOVE action

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -3,3 +3,15 @@ const removeBook = (id) => ({ type: 'REMOVE', id });
 
 const initialState = [];
 
+const changeBooks = (state = initialState, action) => {
+  switch(action.type){
+    case 'ADD':
+      return [...state, action.payload]
+    case 'REMOVE':
+      return state.filter((book) => book.id !== action.id)
+    default:
+      return state;
+  }
+};
+
+export default changeBooks;
