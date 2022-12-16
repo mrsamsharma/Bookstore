c6fded5d85ecf9275ce125f51d24ec689081bbd5
Satyam Sharma <samwfelice@gmail.com>
2022-12-16 03:18:52 -0800

    Create an action creator for getting books from the api

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Header from '../components/Header';
 import Book from '../components/Book';
 import AddBook from '../components/AddBook';
+import { fetchBooks } from '../redux/books/books';
 import '../styles.css';
 
 export default function Books() {
   const books = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchBooks());
+  });
   return (
     <>
       <Header />
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,18 +1,18 @@
+import axios from "axios";
+
 const addBook = (payload) => ({ type: 'ADD', payload });
 const removeBook = (id) => ({ type: 'REMOVE', id });
+const getBooks = (payload) => ({ type: 'GET', payload });
+const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/DwJiw8QxAUA6baUHq2qU/books';
 
-const initialState = [
-  {
-    id: '1',
-    title: 'The Horizon',
-    author: 'Sam Felice',
-  },
-  {
-    id: '2',
-    title: 'The Horizon Part 2',
-    author: 'Sam Felice',
-  },
-];
+const initialState = [];
+
+export const fetchBooks = () => async (dispatch) => {
+  const res = await axios.get(url);
+  const books = [];
+  Object.keys(res.data).map((key) => books.push({ id: key, ...res.data[key][0] }));
+  dispatch(getBooks(books));
+};
 
 export const postBook = (payload) => (dispatch) => {
   dispatch(addBook(payload));
@@ -26,6 +26,8 @@ const changeBooks = (state = initialState, action) => {
   switch (action.type) {
     case 'ADD':
       return [...state, action.payload];
+    case 'GET':
+      return action.payload;
     case 'REMOVE':
       return state.filter((book) => book.id !== action.id);
     default:
